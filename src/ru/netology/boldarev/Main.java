3a285996adea1236eb7252f6bde33a141dfae5eb
Maxim <maxikboldarev13@yandex.ru>
2024-02-15 18:35:26 +0200

    init

--- src/ru/netology/boldarev/Main.java
+++ src/ru/netology/boldarev/Main.java
@@ -1,9 +1,25 @@
 package ru.netology.boldarev;
 
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        String name;
+        int sum;
+        Date currentDate = new Date();
+
         System.out.println("Hello, Max!");
 
+        System.out.println("Введите имя получателя: ");
+        name = scanner.nextLine();
+
+        System.out.println("Введите сумму: ");
+        sum = scanner.nextInt();
+
+        System.out.println("Сумма: " + sum + ", отправлена получателю: " + name + " Дата отправки: " + currentDate);
     }
 }
