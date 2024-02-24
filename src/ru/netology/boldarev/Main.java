c15d9110e341bfbc51cd740cc90f7b0c637f646a
Maxim <maxikboldarev13@yandex.ru>
2024-02-24 09:36:40 +0200

    dz 3

--- src/ru/netology/boldarev/Main.java
+++ src/ru/netology/boldarev/Main.java
@@ -6,20 +6,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        StringBuilder stringBuilder = new StringBuilder();
+
         Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
 
         String name;
         int sum;
-        Date currentDate = new Date();
+        
 
         System.out.println("Hello, Max!");
 
-        System.out.println("Введите имя получателя: ");
-        name = scanner.nextLine();
+        while (count < 5) {
+            Date currentDate = new Date();
 
-        System.out.println("Введите сумму: ");
-        sum = scanner.nextInt();
+            System.out.println("Введите имя получателя: ");
+            name = scanner.next();
 
-        System.out.println("Сумма: " + sum + ", отправлена получателю: " + name + " Дата отправки: " + currentDate);
+            System.out.println("Введите сумму: ");
+            sum = scanner.nextInt();
+
+            stringBuilder.append("Сумма: ")
+                    .append(sum)
+                    .append(", отправлена получателю: ")
+                    .append(name).append(" Дата отправки: ")
+                    .append(currentDate).append("\n");
+
+            count++;
+        }
+
+        String result = stringBuilder.toString();
+
+        System.out.println(result);
     }
 }
